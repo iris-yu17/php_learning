4514d33cd8332cce663afed1a22a79999eaa6fa3
Iris Yu <iris.yu0716@hmail.com>
2020-08-23 14:18:44 +0800

    review update

--- 8-21/4.plus_api.php
+++ 8-21/4.plus_api.php
@@ -2,5 +2,8 @@
 $a = isset($_GET['a']) ? $_GET['a'] : 0;
 $b = isset($_GET['b']) ? $_GET['b'] : 0;
 
-// echo '{"answer":'. ($a+$b). '}';
-printf('{"answer":%s}', $a + $b);
+echo '{"answer":'. ($a+$b).'}';
+
+// %s空格，逗號後面依序帶入%s
+// printf('{"answer":%s}', $a+$b);
+// printf('{"answer":%s%s}', $a,$b);
