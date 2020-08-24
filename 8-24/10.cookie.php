fde8a9f3f6f3b8e331450909be01d6e5875c9f69
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-08-24 16:43:24 +0800

    add

---
+++ 8-24/10.cookie.php
@@ -0,0 +1,27 @@
+<!-- 設cookie，要放在上面 -->
+<?php
+
+setcookie('mycookie', 2);
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    echo $_COOKIE['mycookie'];
+
+    ?>
+
+    <!-- check F12 -> application -->
+</body>
+
+</html>
---
+++ 8-24/11.cookie.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- 前面已設定過就能呼叫 -->
+    <!-- 可在F12 application清除 -->
+    <?php
+
+    echo $_COOKIE['mycookie'];
+
+    ?>
+</body>
+
+</html>
