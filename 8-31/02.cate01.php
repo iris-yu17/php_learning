8a7041999f6c7131b9fa2177e29a1454a57a7e2d
iris-yu17 <69294501+iris-yu17@users.noreply.github.com>
2020-08-31 16:44:31 +0800

    add

--- 8-31/02.cate01.php
+++ 8-31/02.cate01.php
@@ -5,4 +5,27 @@ require __DIR__ . '/../8-25/09.connect_database/01.connect.php';
 // query: 選擇資料
 $rows = $pdo->query("SELECT * FROM `categories`")->fetchAll();
 
+$cates = [];
+
+// 第一層
+// $k: 第幾個object
+// $v: object {"sid": "1", "name": "程式設計","parent_sid": "0" },
+
+foreach ($rows as $k => $v) {
+    if ($v['parent_sid'] == '0') {
+        // push
+        $cates[] = $v;
+    }
+}
+
+// 第二層
+foreach ($cates as $k => $v) {
+    foreach ($rows as $k2 => $v2) {
+        if ($v['sid'] == $v2['parent_sid']) {
+            $cates[$k]['children'][] = $v2;
+        }
+    }
+}
+
+
 echo json_encode($rows, JSON_UNESCAPED_UNICODE);
